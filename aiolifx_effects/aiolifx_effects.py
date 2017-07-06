d8115ab7c1bb5deb47a330b7c8cc5707a9bbc433
Anders Melchiorsen <amelchio@nogoto.net>
2017-07-06 17:35:01 +0200

    Remove passing of the event loop to asyncio.wait

--- aiolifx_effects/aiolifx_effects.py
+++ aiolifx_effects/aiolifx_effects.py
@@ -86,7 +86,7 @@ class Conductor:
             if device.color_zones:
                 for zone in range(0, len(device.color_zones), 8):
                     tasks.append(AwaitAioLIFX().wait(partial(device.get_color_zones, start_index=zone)))
-        yield from asyncio.wait(tasks, loop=self.loop)
+        yield from asyncio.wait(tasks)
 
         for device in participants:
             pre_state = PreState(device)
@@ -109,7 +109,7 @@ class Conductor:
         tasks = []
         for device in devices:
             tasks.append(self.loop.create_task(self._stop_one(device)))
-        yield from asyncio.wait(tasks, loop=self.loop)
+        yield from asyncio.wait(tasks)
 
     @asyncio.coroutine
     def _stop_one(self, device):
@@ -157,7 +157,7 @@ class Conductor:
             tasks = []
             for device in fixup:
                 tasks.append(AwaitAioLIFX().wait(partial(device.set_power, state)))
-            yield from asyncio.wait(tasks, loop=self.loop)
+            yield from asyncio.wait(tasks)
             yield from asyncio.sleep(0.3)
 
         # Power on
@@ -168,7 +168,7 @@ class Conductor:
         for device in fixup:
             for zone in range(0, len(device.color_zones), 8):
                 tasks.append(AwaitAioLIFX().wait(partial(device.get_color_zones, start_index=zone)))
-        yield from asyncio.wait(tasks, loop=self.loop)
+        yield from asyncio.wait(tasks)
 
         # Update pre_state colors
         for device in fixup:
@@ -199,7 +199,7 @@ class LIFXEffect:
             if self.power_on and not device.power_level:
                 tasks.append(self.conductor.loop.create_task(self.poweron(device)))
         if tasks:
-            yield from asyncio.wait(tasks, loop=self.conductor.loop)
+            yield from asyncio.wait(tasks)
 
         yield from self.async_play()
 
